222cf1b9ab08c730deff56053396bad4fdf819e9
alexpagani <alexpaganibr@gmail.com>
2022-05-26 12:56:28 +0200

    Finito modello e impostato la classe context

---
+++ Shop Videogiochi/Data/VideogameShopContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop_Videogiochi.Data
+{
+    public DbSet<Categoria>? Categorie { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Database=Shop_Videogiochi;Integrated Security=True");
+    }
+}
