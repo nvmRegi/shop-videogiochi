9b20a37ee3dd1b3000083abc997ae017df2d7003
alexpagani <alexpaganibr@gmail.com>
2022-05-26 12:59:56 +0200

    Dimenticavo i costruttori del modello

--- Shop Videogiochi/Models/Categoria.cs
+++ Shop Videogiochi/Models/Categoria.cs
@@ -9,9 +9,20 @@ namespace Shop_Videogiochi.Models
     public class Categoria
     {
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
         [Required(ErrorMessage = "Il tema del videogioco")]
-        [StringLength(30,ErrorMessage = "Il tema non può avere più di ")]
+        [StringLength(30, ErrorMessage = "Il tema non può avere più di ")]
         public string Tema { get; set; }
+
+        public Categoria()
+        {
+
+        }
+
+        public Categoria(string Tema)
+        {
+            this.Tema = Tema;
+        }
     }
+
 }
