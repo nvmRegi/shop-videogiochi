0f4f6e8eea9f8bafe2f1412153eef1152ac26f89
Alberto Rosa <alberto.r.93@hotmail.it>
2022-05-26 15:53:01 +0200

    bozza Admin Controller Details

--- Shop Videogiochi/Controllers/AdminController.cs
+++ Shop Videogiochi/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Shop_Videogiochi.Data;
+using Shop_Videogiochi.Models;
 
 namespace Shop_Videogiochi.Controllers
 {
@@ -15,4 +17,47 @@ namespace Shop_Videogiochi.Controllers
             return View();
         }
     }
+
+
+
+
+
+
+
+    /*-------------------------------------read---------------------------------------------------------------------------------------*/
+
+    [HttpGet]
+    public IActionResult Dettagli(int id)
+    {
+
+        using VideogameShopContext db = new VideogameShopContext()
+        {
+            try
+            {
+                Videogioco videogiocoTrovato = db.Videogiochi
+                    .Where(x => x.Id == id)
+                    .Single();
+                return View("Details", videogiocoTrovato);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound("Pacchetto inesistente");
+            }
+
+        }
+    }
+    /*----------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
