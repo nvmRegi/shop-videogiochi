7bd7ae31b019eaec54ac69dc0d2f0c3f4dc99402
alexpagani <alexpaganibr@gmail.com>
2022-05-26 14:30:29 +0200

    Aggiunta dei modelli Categorie, Videogiochi, Ordini nel Context

--- Shop Videogiochi/Data/VideogameShopContext.cs
+++ Shop Videogiochi/Data/VideogameShopContext.cs
@@ -1,9 +1,17 @@
-﻿namespace Shop_Videogiochi.Data
-{
-    public DbSet<Categoria>? Categorie { get; set; }
+﻿using Microsoft.EntityFrameworkCore;
+using Shop_Videogiochi.Models;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Data Source=localhost;Database=Shop_Videogiochi;Integrated Security=True");
+namespace Shop_Videogiochi.Data
+{
+    public class VideogameShopContext : DbContext
+    { 
+        DbSet<Categoria> Categorie { get; set; }
+        DbSet<Videogioco> Videogiochi { get; set; }
+        DbSet<Ordine> Ordini { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=Shop_Videogiochi;Integrated Security=True");
+        }
     }
 }
