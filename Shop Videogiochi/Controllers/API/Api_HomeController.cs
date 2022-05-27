6b3ba952b8d3d631e9af938748e41380fe6d5132
Alberto Rosa <alberto.r.93@hotmail.it>
2022-05-27 11:38:17 +0200

    file api generato

---
+++ Shop Videogiochi/Controllers/API/Api_HomeController.cs
@@ -0,0 +1,50 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Shop_Videogiochi.Data;
+//using Shop_Videogiochi.Models;
+
+//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+//namespace Shop_Videogiochi.Controllers.API
+//{
+
+//    //api per i videogiochi per l'home 
+
+
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class Api_HomeController : ControllerBase
+//    {
+//        // GET: api/<Api_HomeController>
+//        [HttpGet]
+//        public IEnumerable<string> Get()
+//        {
+//            return new string[] { "value1", "value2" };
+//        }
+
+//        // GET api/<Api_HomeController>/5
+//        [HttpGet("{id}")]
+//        public string Get(int id)
+//        {
+//            return "value";
+//        }
+
+//        // POST api/<Api_HomeController>
+//        [HttpPost]
+//        public void Post([FromBody] string value)
+//        {
+//        }
+
+//        // PUT api/<Api_HomeController>/5
+//        [HttpPut("{id}")]
+//        public void Put(int id, [FromBody] string value)
+//        {
+//        }
+
+//        //// DELETE api/<Api_HomeController>/5
+//        //[HttpDelete("{id}")]
+//        //public void Delete(int id)
+//        //{
+//        //}
+//    }
+//}
