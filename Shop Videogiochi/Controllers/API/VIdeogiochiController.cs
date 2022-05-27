4e675816ef1c10ae42822c7b58cd8509baeb75c4
Alberto Rosa <alberto.r.93@hotmail.it>
2022-05-27 11:56:48 +0200

    Api_HomeController > VideogiochiController

--- Shop Videogiochi/Controllers/API/VIdeogiochiController.cs
+++ Shop Videogiochi/Controllers/API/VIdeogiochiController.cs
@@ -13,7 +13,7 @@
 
 //    [Route("api/[controller]")]
 //    [ApiController]
-//    public class Api_HomeController : ControllerBase
+//    public class VideogiochiController : ControllerBase
 //    {
 //        // GET: api/<Api_HomeController>
 //        [HttpGet]
@@ -22,26 +22,26 @@
 //            return new string[] { "value1", "value2" };
 //        }
 
-//        // GET api/<Api_HomeController>/5
+//        // GET api/<VideogiochiController>/5
 //        [HttpGet("{id}")]
 //        public string Get(int id)
 //        {
 //            return "value";
 //        }
 
-//        // POST api/<Api_HomeController>
+//        // POST api/<VideogiochiController>
 //        [HttpPost]
 //        public void Post([FromBody] string value)
 //        {
 //        }
 
-//        // PUT api/<Api_HomeController>/5
+//        // PUT api/<VideogiochiController>/5
 //        [HttpPut("{id}")]
 //        public void Put(int id, [FromBody] string value)
 //        {
 //        }
 
-//        //// DELETE api/<Api_HomeController>/5
+//        //// DELETE api/<VideogiochiController>/5
 //        //[HttpDelete("{id}")]
 //        //public void Delete(int id)
 //        //{
