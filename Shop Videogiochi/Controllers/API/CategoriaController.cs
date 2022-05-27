26e7552b8c55f3361e4b264ecde3b76dfdc8d498
Davide Scriboni <davidescriboni94@gmail.com>
2022-05-27 14:40:09 +0200

    categoria controller

---
+++ Shop Videogiochi/Controllers/API/CategoriaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop_Videogiochi.Data;
+using Shop_Videogiochi.Models;
+
+namespace Shop_Videogiochi.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            List<Videogioco> videoGiochiPerCategoria = new List<Videogioco>();
+            using (VideogameShopContext db= new VideogameShopContext())
+            {
+                
+
+                if(id != null)
+                {
+                    videoGiochiPerCategoria = db.Videogiochi.Where(videogioco => videogioco.CategoriaId == id).ToList();
+                    return Ok(videoGiochiPerCategoria);
+                }
+                return BadRequest();
+
+                
+                
+            }
+        }
+    }
+}
