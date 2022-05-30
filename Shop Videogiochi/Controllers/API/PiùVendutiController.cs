a818f8c4ce031f18f510441498f52c292183dc31
LucaVanossi <vanossi.luca13@gmail.com>
2022-05-30 14:34:27 +0200

    Form ok

---
+++ Shop Videogiochi/Controllers/API/PiùVendutiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop_Videogiochi.Data;
+using Shop_Videogiochi.Models;
+
+//namespace Shop_Videogiochi.Controllers.API
+//{
+//    [Route("api/[controller]/[action]")]
+//    [ApiController]
+//    public class PiùVendutiController : ControllerBase
+//    {
+//        [HttpGet]
+//        public IActionResult Get()
+//        {
+//            List<Videogioco> listaPiuVenduti = new List<Videogioco>();
+//            using (VideogameShopContext db = new VideogameShopContext())
+//            {
+                
+//               listaPiuVenduti = db.Ordini.FromSqlRaw("")
+             
+//            }
+//        }
+//    }
+//}
