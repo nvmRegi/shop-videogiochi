7541c5a0e8c049120986c7d623af3b9927bea1dc
Alberto Rosa <alberto.r.93@hotmail.it>
2022-05-31 16:27:38 +0200

    prova mipiace hover

---
+++ SQLQuery2.sql
@@ -0,0 +1,1 @@
+﻿SELECT Videogiochi.* FROM Videogiochi INNER JOIN Ordini ON Videogiochi.Id = Ordini.VideogiocoId WHERE MONTH(data) = MONTH(DATEADD(month, -1, GETDATE())) AND YEAR(data) = YEAR(GETDATE()) GROUP BY Videogiochi.Id, Videogiochi.Nome, Videogiochi.Descrizione, Videogiochi.Prezzo, Videogiochi.Foto, Videogiochi.CategoriaId, Videogiochi.MiPiace ORDER BY SUM(Quantità) DESC
