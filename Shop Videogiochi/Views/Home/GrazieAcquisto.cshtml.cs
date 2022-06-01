bec04e828ddee42437118c558e66574a582259f6
nvmRegi <cabael.regine@gmail.com>
2022-06-01 15:01:01 +0200

    Nuova vista grazie, non finito

---
+++ Shop Videogiochi/Views/Home/GrazieAcquisto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shop_Videogiochi.Views.Home
+{
+    public class GrazieAcquistoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
