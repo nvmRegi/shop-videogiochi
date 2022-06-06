ff89745e51841a21c4eef3b10fd01b75d5e97caf
regi <cabael.regine@gmail.com>
2022-06-06 16:13:21 +0200

    Sistemato categorie nei mi piace

--- Shop Videogiochi/Controllers/API/CategoriaController.cs
+++ Shop Videogiochi/Controllers/API/CategoriaController.cs
@@ -10,13 +10,21 @@ namespace Shop_Videogiochi.Controllers.API
     public class CategoriaController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(int? idCategoria)
         {
             List<Categoria> categorie = new List<Categoria>();
             using(VideogameShopContext db = new VideogameShopContext())
             {
-                categorie = db.Categorie.ToList();
-                return Ok(categorie);
+                if(idCategoria != null && idCategoria != -1)
+                {
+                    Categoria categoria = db.Categorie.Where(categoria => categoria.Id == idCategoria).FirstOrDefault();
+                    return Ok(categoria);
+                }
+                else
+                {
+                    categorie = db.Categorie.ToList();
+                    return Ok(categorie);
+                }
             }
         }
     }
